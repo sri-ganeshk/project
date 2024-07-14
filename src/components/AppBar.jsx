92ae89f91931a4e7b92a5f241d82dafabce2b926
nithinmouli <139532214+nithinmouli@users.noreply.github.com>
2024-07-14 21:54:16 +0530

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -43,10 +43,10 @@ const AppBar = () => {
       </div>
       {menuOpen && (
         <div className="md:hidden absolute top-16 left-0 w-full bg-black flex flex-col items-center space-y-4 py-4 text-lg">
-          <button onClick={() => navigate("/")}>Home</button>
-          <button onClick={() => handleNavigation("popular")}>Popular</button>
-          <button onClick={() => handleNavigation("top-rated")}>Top Rated</button>
-          <button onClick={() => handleNavigation("upcoming-movies")}>Upcoming</button>
+          <button className = "hover:bg-red-900" onClick={() => navigate("/")}>Home</button>
+          <button className = "hover:bg-red-900" onClick={() => handleNavigation("popular")}>Popular</button>
+          <button className = "hover:bg-red-900" onClick={() => handleNavigation("top-rated")}>Top Rated</button>
+          <button className = "hover:bg-red-900" onClick={() => handleNavigation("upcoming-movies")}>Upcoming</button>
           <button
             className="bg-red-700 text-white px-5 py-2 rounded-full hover:bg-red-900"
             onClick={() => handleNavigation("signin")}
